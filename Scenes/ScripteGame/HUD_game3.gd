a4de59aa0c09d9fb4ebdd2f27e8d511793516d58
desaiharsh0911-del <desaiharsh0911@gmail.com>
2026-03-01 08:05:39 -0500

    fixed

--- Scenes/ScripteGame/HUD_game3.gd
+++ Scenes/ScripteGame/HUD_game3.gd
@@ -6,6 +6,7 @@ func _ready():
 	$Label.set_text(str(GAME.get_FCC()))
 	$Label2.set_text("Bet : " + str(owner.get_bet()))
 	owner.connect("score_update", _update_score)
+	owner.connect("npc_score_update", _update_npc_score)
 
 func _update_money(new_value):
 	$Label.set_text(str(new_value))
@@ -15,3 +16,6 @@ func _update_bet(new_value):
 
 func _update_score(new_value):
 	$Label3.set_text("Score : " + str(new_value))
+
+func _update_npc_score(new_value):
+	$Label4.set_text("Jack : " + str(new_value))
